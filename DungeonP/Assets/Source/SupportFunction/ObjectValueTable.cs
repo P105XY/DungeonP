cc515578ddda43a990aabf67670a4f927460e520
P105XY <p105xy85@gmail.com>
2025-01-12 16:41:08 +0900

    인벤토리 슬롯 크기 저장 코드 추가.

---
+++ DungeonP/Assets/Source/SupportFunction/ObjectValueTable.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ObjectDataTable
+{
+    public static readonly float InventorySlotSize = 29.5f;
+}
